1edce6315a1624389a9ee4c87053861e43c82771
drew654 <drewpusey654@gmail.com>
2024-10-02 12:55:33 -0500

    Navbar icons

--- ui/src/app/components/TopNavbar.tsx
+++ ui/src/app/components/TopNavbar.tsx
@@ -1,7 +1,14 @@
+import { Cog6ToothIcon, UserIcon } from "@heroicons/react/24/solid";
+
 export default function TopNavbar() {
   return (
-    <nav className="p-4">
-      <p>Top Navbar</p>
+    <nav className="flex flex-row-reverse p-4">
+      <div className="px-1">
+        <Cog6ToothIcon className="size-6" />
+      </div>
+      <div className="px-1">
+        <UserIcon className="size-6" />
+      </div>
     </nav>
   );
 }
