2a185cdb238aecefd5c8a338ecb1f3e24c6414d4
Deric Le <freeform2745@gmail.com>
2024-10-07 13:38:12 -0500

    update icons on sidebar

--- ui/src/constants.tsx
+++ ui/src/constants.tsx
@@ -11,17 +11,17 @@ export const SIDENAV_ITEMS: SideNavItem[] = [
   {
     title: 'Students',
     path: '/students',
-    icon: <Icon icon="lucide:folder" width="24" height="24" />,
+    icon: <Icon icon="lucide:users" width="24" height="24" />,
   },
   {
     title: 'Assignments',
     path: '/assignments',
-    icon: <Icon icon="lucide:mail" width="24" height="24" />,
+    icon: <Icon icon="lucide:folder" width="24" height="24" />,
   },
   { 
     title: 'Machines',
     path: '/machines',
-    icon: <Icon icon="lucide:settings" width="24" height="24" />,
+    icon: <Icon icon="lucide:computer" width="24" height="24" />,
     subMenuItems: [
       { title: 'Containers', path: '/machines/containers' },
       { title: 'Images', path: '/machines/images' },
