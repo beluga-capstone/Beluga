0659fb3c51a12ca35ac5f5b1c8fda2d6012c7167
Drew Pusey <drewpusey654@gmail.com>
2024-10-09 17:06:44 -0500

    on-surface color

--- ui/src/app/assignments/page.tsx
+++ ui/src/app/assignments/page.tsx
@@ -2,12 +2,12 @@ export default function Assignments() {
   return (
     <>
       <span className="font-bold text-4xl">Assignments</span>
-      <div className="border-dashed border w-full h-12 rounded-lg"></div>
-      <div className="border-dashed border w-full h-64 rounded-lg"></div>
-      <div className="border-dashed border w-full h-64 rounded-lg"></div>
-      <div className="border-dashed border w-full h-64 rounded-lg"></div>
-      <div className="border-dashed border w-full h-64 rounded-lg"></div>
-      <div className="border-dashed border w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-on-surface w-full h-12 rounded-lg"></div>
+      <div className="border-dashed border border-on-surface w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-on-surface w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-on-surface w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-on-surface w-full h-64 rounded-lg"></div>
+      <div className="border-dashed border border-on-surface w-full h-64 rounded-lg"></div>
     </>
   );
 }
--- ui/tailwind.config.ts
+++ ui/tailwind.config.ts
@@ -12,6 +12,7 @@ const config: Config = {
         background: "var(--background)",
         foreground: "var(--foreground)",
         surface: "var(--surface)",
+        "on-surface": "var(--on-surface)",
       },
     },
   },
