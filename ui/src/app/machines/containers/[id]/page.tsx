cfe255fd23ff189bb1948a439be66d01deee718f
Deric Le <dericle77@gmail.com>
2024-10-15 22:14:18 -0500

    display the container info in the dynamic page

--- ui/src/app/machines/containers/[id]/page.tsx
+++ ui/src/app/machines/containers/[id]/page.tsx
@@ -1,16 +1,61 @@
 "use client";
 
-import { useParams } from 'next/navigation';
+import { useEffect, useState } from "react";
+import { useContainers } from "@/hooks/useContainers";
+import { Container } from "@/types";
 
-const ContainerPage = () => {
-  const params = useParams();
+import ContainerPageInfo from "@/components/ContainerPageInfo";
 
-    //   todo: check if container exists
-  return (
-    <div className="container mx-auto p-4">
-      <h1 className="font-bold text-4xl mb-6">Container {params.id}</h1>
-    </div>
-  );
+const ContainerPage = ({ params }: { params: { id: string } }) => {
+    const { containers } = useContainers();
+    const [container, setContainer] = useState<Container | null>(null);
+    const [loading, setLoading] = useState(true); 
+    const [notFound, setNotFound] = useState(false); 
+    const containerId = parseInt(params.id, 10);
+
+    useEffect(() => {
+        const foundContainer = containers.find((c) => c.id === containerId);
+
+        if (foundContainer) {
+            setContainer(foundContainer);
+            setLoading(false);
+        } else {
+            const timeout = setTimeout(() => {
+                setLoading(false);
+                setNotFound(true);
+            }, 5000);
+
+            return () => clearTimeout(timeout);
+        }
+    }, [containerId, containers]);
+
+    if (loading) {
+        return (
+            <div className="container mx-auto p-4">
+                <h1 className="font-bold text-4xl mb-6">Loading...</h1>
+            </div>
+        );
+    }
+
+    if (notFound) {
+        return (
+            <div className="container mx-auto p-4">
+                <h1 className="font-bold text-4xl mb-6">Container not found</h1>
+            </div>
+        );
+    }
+
+    if (container) {
+        return (
+            <div className="container mx-auto p-4">
+                <h1 className="font-bold text-4xl mb-6">Container "{container.name}"</h1>
+                <ContainerPageInfo container={container} />
+
+                {/* display container component here */}
+
+            </div>
+        );
+    }
 };
 
 export default ContainerPage;
