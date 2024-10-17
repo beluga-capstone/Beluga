d12639a9f72d4b0b0315b1dccf4b46267dcc7696
cherry13245 <cherrytree1324@tamu.edu>
2024-10-17 14:54:23 -0500

    register blueprints

--- api/src/__init__.py
+++ api/src/__init__.py
@@ -10,6 +10,18 @@ def create_app():
     db.init_app(app)
 
     with app.app_context():
+        from src.blueprints import assignment, container, course_enrollment, courses, dependencies, image, role, submissions, term, users
+        app.register_blueprint(assignment.assignment_bp)
+        app.register_blueprint(container.container_bp)
+        app.register_blueprint(course_enrollment.enrollment_bp)
+        app.register_blueprint(courses.course_bp)
+        app.register_blueprint(dependencies.dependencies_bp)
+        app.register_blueprint(image.image_bp)
+        app.register_blueprint(role.role_bp)
+        app.register_blueprint(submissions.submission_bp)
+        app.register_blueprint(term.term_bp)
+        app.register_blueprint(users.users_bp)
+
         db.create_all()
 
     return app
