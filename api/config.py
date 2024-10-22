6a962a1f749510c17b6ccec629bae2e1fb52a267
JelBell1324 <jeffthegreat66@gmail.com>
2024-10-22 17:03:28 -0500

    database uri

--- api/config.py
+++ api/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://username:password@localhost:port/DBNAME')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://root:temppassword@localhost:5432/beluga')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
