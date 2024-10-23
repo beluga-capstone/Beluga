5aaef31567f591fb76aa1218914d3353218b45be
Drew Pusey <drewpusey654@gmail.com>
2024-10-23 09:56:22 -0500

    Hidden user bulk actions

--- ui/src/components/UsersBulkActions.tsx
+++ ui/src/components/UsersBulkActions.tsx
@@ -1,22 +1,23 @@
 import Button from "./Button";
 
 interface BulkActionsProps {
-    selectedCount: number;
-    onDelete:()=>null;
+  selectedCount: number;
+  onDelete: () => null;
 }
 
-const BulkActions: React.FC<BulkActionsProps> = ({ selectedCount, onDelete }) => (
-    <div className="mb-4 flex items-center">
-        <span className="mr-4">{selectedCount} users selected</span>
-        <div className="ml-auto">
-            <Button
-            onClick={onDelete}
-            className="bg-red-700 text-white px-4 py-2 rounded"
-            > 
-            Delete Selected
-            </Button>
-        </div>
+const BulkActions: React.FC<BulkActionsProps> = ({ selectedCount, onDelete }) =>
+  selectedCount > 0 && (
+    <div className="flex items-center">
+      <span className="mr-4">{selectedCount} users selected</span>
+      <div className="ml-auto">
+        <Button
+          onClick={onDelete}
+          className="bg-red-700 text-white px-4 py-2 rounded"
+        >
+          Delete Selected
+        </Button>
+      </div>
     </div>
-);
+  );
 
 export default BulkActions;
