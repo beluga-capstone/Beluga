cceddbb3803f8bce826a80e68f79ce67d241a75c
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2024-10-23 02:48:13 -0500

    Add extra config option for later

--- api/config.py
+++ api/config.py
@@ -3,3 +3,14 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://root:temppassword@localhost:5432/beluga')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+class TestingConfig(Config):
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.getenv('TEST_DATABASE_URI', 'sqlite:///:memory:')  # Use in-memory SQLite for tests
+
+
+config_options = {
+    #'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    #'production': ProductionConfig,
+}
