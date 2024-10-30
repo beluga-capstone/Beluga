b663fa52a64c3161a7aff46f11d34a783bd69ee9
drew654 <drewpusey654@gmail.com>
2024-10-30 14:15:15 -0500

    Assignments student view

--- ui/src/app/assignments/[assignmentId]/page.tsx
+++ ui/src/app/assignments/[assignmentId]/page.tsx
@@ -1,11 +1,14 @@
 "use client";
 
+import { ROLES } from "@/constants";
 import { useAssignments } from "@/hooks/useAssignments";
 import { useContainers } from "@/hooks/useContainers";
+import { useProfile } from "@/hooks/useProfile";
 import { Edit2 } from "lucide-react";
 import Link from "next/link";
 
 const AssignmentPage = ({ params }: { params: { assignmentId: string } }) => {
+  const { profile } = useProfile();
   const { assignments } = useAssignments();
   const assignment = assignments.find(
     (assignment) => assignment.id === parseInt(params.assignmentId, 10)
@@ -19,9 +22,11 @@ const AssignmentPage = ({ params }: { params: { assignmentId: string } }) => {
     <div className="container mx-auto p-4">
       <div className="mb-8 flex items-center">
         <h1 className="font-bold text-4xl">{assignment?.title}</h1>
-        <Link href={`/assignments/edit/${assignment?.id}`} className="px-6">
-          <Edit2 size={24} />
-        </Link>
+        {profile?.role !== ROLES.STUDENT && (
+          <Link href={`/assignments/edit/${assignment?.id}`} className="px-6">
+            <Edit2 size={24} />
+          </Link>
+        )}
       </div>
       <div className="flex justify-between items-center">
         <div className="flex-row">
@@ -67,11 +72,13 @@ const AssignmentPage = ({ params }: { params: { assignmentId: string } }) => {
           </p>
         </>
       )}
-      <p className="text-blue-500 py-8">
-        <Link href={`/assignments/${assignment?.id}/submissions`}>
-          View Submissions
-        </Link>
-      </p>
+      {profile?.role !== ROLES.STUDENT && (
+        <p className="text-blue-500 py-8">
+          <Link href={`/assignments/${assignment?.id}/submissions`}>
+            View Submissions
+          </Link>
+        </p>
+      )}
     </div>
   );
 };
