b4fa0da2cfe087677188059cf31631e09db9e421
JelBell1324 <jeffthegreat66@gmail.com>
2024-10-31 17:18:33 -0500

    config

--- api/config.py
+++ api/config.py
@@ -4,7 +4,7 @@ class BaseConfig:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://root:temppassword@localhost:5432/beluga')    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-class TestingConfig(Config):
+class TestingConfig:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'postgresql://postgres:temppassword@db:5432/your_db')
     TESTING = True
 
