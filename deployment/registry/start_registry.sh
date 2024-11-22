25119a281c06977c5bff5e2e47bf3976919121e0
Minh Dao Nguyen <90809666+Minh-Dao-Nguyen@users.noreply.github.com>
2024-11-22 08:36:36 -0600

    Fix run path

--- deployment/registry/start_registry.sh
+++ deployment/registry/start_registry.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # build
-docker build -t beluga-registry ./deployment/registry/
+docker build -t beluga-registry .
 
 # deploy
 docker run -d --name beluga-registry -p 8000:5000 beluga-registry
