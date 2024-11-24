254bb83c153800ac98e01bed1fdfc2af76eb56ba
Bode-Raymond <89925791+Bode-Raymond@users.noreply.github.com>
2024-11-23 18:37:46 -0600

    docker setup cli

---
+++ deployment/beluga/src/beluga/plugins/docker/cli.py
@@ -0,0 +1,40 @@
+from enum import StrEnum
+
+from beluga.core.cli import CliGroup, Beluga, beluga_cli
+from beluga.plugins.docker import docker
+
+cli_group = CliGroup(beluga_cli.subparsers, "docker")
+
+
+class DeployType(StrEnum):
+    registry = "registry"
+    baseimg = "baseimg"
+
+
+class Docker(Beluga):
+    """
+    Helper functions for administration of containers.
+    """
+
+    __slots__ = ("_docker")
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._docker = docker.Docker(self._workspace)
+
+    @cli_group.option(
+        "--deploy", "-d", help="type of service to deploy", choices=DeployType
+    )
+    @cli_group.command()
+    def deploy(self, deploy_type: DeployType):
+        """
+        Deploy images/containers
+        """
+        deploy_type = DeployType(deploy_type)
+        self._docker.setup_context()
+
+        match deploy_type:
+            case DeployType.registry:
+                self._docker.setup_registry()
+            case DeployType.baseimg:
+                self._docker.setup_base_image()
---
+++ deployment/beluga/src/beluga/plugins/docker/docker.py
@@ -0,0 +1,71 @@
+import textwrap
+from typing import Iterable
+
+from beluga.common import run
+from beluga.core.schema.workspace import (
+    WorkspaceSchema,
+    ContainerMachine,
+)
+
+
+class Docker:
+    __slots__ = ("_workspace", "_registry", "_frontend")
+
+    _workspace: WorkspaceSchema
+    _registry: ContainerMachine
+
+    def __init__(self, workspace: WorkspaceSchema):
+        self._workspace = workspace
+        self._registry = self._workspace.virtual_machines._registry
+
+    def setup_context(self):
+        for vm in self._workspace.virtual_machines.all():
+            run.context.run_cmd(
+                textwrap.dedent(
+                    f"""
+                    docker context create {vm.name}
+                        --docker "host=ssh://{vm.name}"
+                    """
+                )
+            )
+        
+    def setup_registry(self):
+        self._pull_image(
+            name="registry",
+            image="registry:2",
+            ports=["5000"],
+            context=self._registry.name,
+        )
+
+    def setup_base_image(self, vm_name: str, containers: Iterable[str] = [], verbose: bool = False):
+        pass
+
+    def _pull_image(
+        self,
+        name: str,
+        image: str,
+        ports: Iterable[str],
+        context: str,
+        runtime: str = "",
+    ):
+        ports = " ".join((f"-p {p}" if ":" in p else f"-p {p}:{p}" for p in ports))
+
+        run.context.run_cmd(f"docker pull {image}")
+        run.context.run_cmd(
+            f"docker save {image} | gzip | docker --context {context} load", shell=True
+        )
+        run.context.run_cmd(
+            f"docker --context {context} run {runtime} --restart=always -d {ports} --name {name} {image}"
+        )
+
+    def _push_image(
+        self,
+        context: str,
+        registry: str,
+        image: str,
+    ):
+        tag = f"{registry}{image}"
+        run.context.run_cmd(f"docker image tag {image} {tag}")
+        run.context.run_cmd(f"docker push {tag}")
+        run.context.run_cmd(f"docker --context {context} pull {tag}")
+
