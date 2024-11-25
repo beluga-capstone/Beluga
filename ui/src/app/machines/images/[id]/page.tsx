cfd4f49553426d76a6b41b95ac3ca6e0b5efd090
Deric Le <dericle77@gmail.com>
2024-11-24 22:11:32 -0600

    images thing

--- ui/src/app/machines/images/[id]/page.tsx
+++ ui/src/app/machines/images/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
-import { useParams } from "next/navigation";
 import { useImageData } from "@/hooks/useImageData";
 import { useImages } from "@/hooks/useImages";
 import ImageAssignmentList from "@/components/ImageAssignmentList";
