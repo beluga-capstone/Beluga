52c327ac4ffa3dd4094aac5d8b6ee2f1bbcda162
drew654 <drewpusey654@gmail.com>
2024-11-26 14:09:44 -0600

    Hide add image button for student

--- ui/src/app/machines/images/page.tsx
+++ ui/src/app/machines/images/page.tsx
@@ -1,11 +1,13 @@
 "use client";
 import React from "react";
 import { useRouter } from "next/navigation";
-import { CheckSquare, Plus, Square,StopCircle } from "lucide-react";
+import { CheckSquare, Plus, Square, StopCircle } from "lucide-react";
 import ImageItem from "@/components/ImageItem";
 import CheckBox from "@/components/CheckBox";
 import Link from "next/link";
 import { useImages } from "@/hooks/useImages";
+import { useProfile } from "@/hooks/useProfile";
+import { ROLES } from "@/constants";
 
 export default function Images() {
   const {
@@ -16,30 +18,33 @@ export default function Images() {
     selectAllImages,
   } = useImages();
 
+  const { profile } = useProfile();
   const router = useRouter();
 
   return (
     <div className="container mx-auto p-4">
       <h1 className="font-bold text-4xl mb-6">Images</h1>
       <div className="mb-4 flex items-center">
-        <Link href="/machines/images/new">
-          <button className="bg-blue-500 text-white px-4 py-2 rounded flex items-center">
-            <Plus className="mr-2" /> Add Image
-          </button>
-        </Link>
+        {profile?.role_id !== ROLES.STUDENT && (
+          <Link href="/machines/images/new">
+            <button className="bg-blue-500 text-white px-4 py-2 rounded flex items-center">
+              <Plus className="mr-2" /> Add Image
+            </button>
+          </Link>
+        )}
 
         {selectedImageIds.length > 0 && (
           <button
             onClick={deleteSelectedImages}
             className="bg-red-700 text-white ml-2 px-4 py-2 rounded flex items-center"
           >
-            <StopCircle className="mr-2"/>
+            <StopCircle className="mr-2" />
             <span>Delete Selected</span>
           </button>
         )}
       </div>
 
-      <br/>
+      <br />
       {images.length > 0 && (
         <div className="flex items-center mb-4">
           <button
